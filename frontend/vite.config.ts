9fb19fcfc405c16d0501130a8d2fb760d492416c
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-04-07 20:15:56 -0300

    Install ParaglideJS

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,18 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit(), devtoolsJson()] });
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		devtoolsJson(),
+		paraglideVitePlugin({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide',
+			strategy: ['localStorage', 'preferredLanguage', 'globalVariable', 'baseLocale']
+		})
+	]
+});
