53f2589c776f960f994cd82b81812283977312d0
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-04-08 05:56:17 -0300

    Fix anti-vulnerability bug

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -17,7 +17,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		// Due to the lack of static domains and IPs in AWS, ignore all rational security measures :)
+		csrf: { checkOrigin: false }
 	}
 };
 
