31ab38d04268a7f0d361b51ed577b975e88237b0
mquesada02 <mquesada@itba.edu.ar>
2026-04-08 18:23:11 -0300

    more fixes

--- frontend/src/routes/list/+page.server.ts
+++ frontend/src/routes/list/+page.server.ts
@@ -1,8 +1,14 @@
-import type { PageServerLoad } from './$types';
-
-import { getAllCampaigns } from '$lib/server/dynamo';
+import { getAllCampaigns } from "$lib/server/dynamo";
+import type { PageServerLoad } from "./$types";
 
 export const load = (async ({ parent }) => {
 	const { user } = await parent();
-	return await getAllCampaigns(user);
+
+	try {
+		const res = await getAllCampaigns(user);
+		return { Items: res.Items ?? [] };
+	} catch (err) {
+		console.error("getAllCampaigns failed", err);
+		return { Items: [] };
+	}
 }) satisfies PageServerLoad;
