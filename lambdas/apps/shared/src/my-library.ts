8668f34dfff0c756d568c62cc9dd09b8f9212412
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-05-19 01:38:12 -0300

    Finishing touches to lambdas

---
+++ lambdas/apps/shared/src/my-library.ts
@@ -0,0 +1,3 @@
+export function hello_world() {
+    return "Hi";
+}
--- lambdas/apps/template-js/src/handler.ts
+++ lambdas/apps/template-js/src/handler.ts
@@ -1,10 +1,12 @@
 import type { APIGatewayProxyHandlerV2 } from "aws-lambda";
 
+import { hello_world } from "shared/src/my-library";
+
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
     if (event.requestContext.http.method.toUpperCase() === "GET") {
         return {
             statusCode: 200,
-            body: "<h1>Hi</h1>"
+            body: `<h1>${hello_world()}</h1>`
         };
     }
 
