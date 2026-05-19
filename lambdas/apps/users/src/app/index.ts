c4dd916634206769c7fceb77ac64475858b7d2a5
agussang <msanguinetti@itba.edu.ar>
2026-05-19 20:51:30 -0300

    fix(iapi-users): user info is now retrieved from token"

--- lambdas/apps/users/src/app/index.ts
+++ lambdas/apps/users/src/app/index.ts
@@ -10,18 +10,10 @@ app.use(express.json());
 app.route("/me")
     // Only return user id if logged in
     .get(authenticated, async (_req, res) => {
-        const id = res.locals.userId;
-        const results = await pool.query("SELECT * FROM users WHERE id = $1", [
-            id
-        ]);
-
-        const user = results.rows[0];
-
-        if (!user) {
-            return res.sendStatus(404);
-        }
-
-        res.send(user);
+        res.json({
+            id: res.locals.userId,
+            email: res.locals.email,
+        });
     });
 
 export default app;
