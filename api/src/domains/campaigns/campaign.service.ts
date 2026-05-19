a445388cdc33fa603b88ba7f967290afdadac781
agussang <msanguinetti@itba.edu.ar>
2026-05-19 01:57:45 -0300

    feat: changed campaign repo

--- api/src/domains/campaigns/campaign.service.ts
+++ api/src/domains/campaigns/campaign.service.ts
@@ -1,24 +1,27 @@
 import type { Campaign, CampaignInput } from "./campaign.types.js";
 import type { ICampaignRepository } from "./interfaces/campaign.repository.js";
 import type { ICampaignService } from "./interfaces/campaign.service.js";
+import type { IReportRepository } from "../reports/interfaces/report.repository.js";
 
 export class CampaignService implements ICampaignService {
-	private repo: ICampaignRepository;
+	private campaignRepo: ICampaignRepository;
+	private reportRepo: IReportRepository;
 
-	constructor(repo: ICampaignRepository) {
-		this.repo = repo;
+	constructor(campaignRepo: ICampaignRepository, reportRepo: IReportRepository) {
+		this.campaignRepo = campaignRepo;
+		this.reportRepo = reportRepo;
 	}
 
 	async list(userId: string): Promise<Campaign[]> {
-		return this.repo.findAll(userId);
+		return this.campaignRepo.findAll(userId);
 	}
 
 	async get(userId: string, name: string): Promise<Campaign | null> {
-		return this.repo.findOne(userId, name);
+		return this.campaignRepo.findOne(userId, name);
 	}
 
 	async create(userId: string, input: CampaignInput): Promise<void> {
 		const { campaign, topics, start, end } = input;
-		await this.repo.save({ end, name: campaign, start, topics, userId });
+		await this.campaignRepo.save({ end, name: campaign, start, topics, userId });
 	}
 }
