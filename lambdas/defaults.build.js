847e69ff136b795069ed660626a06cfbd0c2b1ac
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-05-20 19:06:16 -0300

    Idc

--- lambdas/defaults.build.js
+++ lambdas/defaults.build.js
@@ -2,9 +2,12 @@ export default /** @type {const} */ ({
 	alias: {
 		"@shared": "shared/src",
 	},
+	banner: {
+		js: "import { createRequire } from 'module'; const require = createRequire(import.meta.url);",
+	},
 	bundle: true,
-	external: /** @type {string[]} */ (["@aws-sdk/*", "http", "express"]),
-	format: "cjs",
+	external: /** @type {string[]} */ (["@aws-sdk/*"]),
+	format: "esm",
 	minify: true,
 	outdir: "./dist",
 	platform: "node",
