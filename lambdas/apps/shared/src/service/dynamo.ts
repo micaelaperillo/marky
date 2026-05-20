d9c696f731fce27b3fb1a8c692ef019b4947901a
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-05-20 02:13:57 -0300

    Nit

--- lambdas/apps/shared/src/service/dynamo.ts
+++ lambdas/apps/shared/src/service/dynamo.ts
@@ -3,14 +3,14 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { env } from "@shared/config";
 
 export const dynamo = new DynamoDBClient({
-  region: env.aws.region,
-  endpoint: env.aws.dynamoEndpoint,
-  ...(env.nodeEnv === "development" && {
-    credentials: {
-      accessKeyId: "local",
-      secretAccessKey: "local"
-    }
-  })
+    region: env.aws.region,
+    endpoint: env.aws.dynamoEndpoint,
+    ...(!env.isProduction && {
+        credentials: {
+            accessKeyId: "local",
+            secretAccessKey: "local"
+        }
+    })
 });
 
 export const TABLE = env.aws.dynamoTable;
