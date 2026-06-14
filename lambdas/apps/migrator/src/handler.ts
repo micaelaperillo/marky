5ebe60e9fb82c9ddcea76f181e6db2503527f159
agussang <msanguinetti@itba.edu.ar>
2026-06-14 03:00:34 -0300

    fix: updating lambda migrator campaign schema

--- lambdas/apps/migrator/src/handler.ts
+++ lambdas/apps/migrator/src/handler.ts
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS campaigns (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   user_sub TEXT NOT NULL,
   name TEXT NOT NULL,
-  start_date DATE NOT NULL,
-  end_date DATE NOT NULL,
+  start_date TIMESTAMPTZ NOT NULL,
+  end_date TIMESTAMPTZ NOT NULL,
   topics TEXT[] NOT NULL,
   CONSTRAINT campaigns_user_name_unique UNIQUE (user_sub, name)
 );
