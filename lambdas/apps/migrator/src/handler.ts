09f56a2a683d7f8942ce3d2cca5c6a98b8db56d5
agussang <msanguinetti@itba.edu.ar>
2026-06-16 22:58:35 -0300

    fix: migrator create table if not exists sql

--- lambdas/apps/migrator/src/handler.ts
+++ lambdas/apps/migrator/src/handler.ts
@@ -8,7 +8,9 @@ CREATE TABLE IF NOT EXISTS campaigns (
   name TEXT NOT NULL,
   start_date TIMESTAMPTZ NOT NULL,
   end_date TIMESTAMPTZ NOT NULL,
+  frequency_min INTEGER NOT NULL DEFAULT 60,
   topics TEXT[] NOT NULL,
+
   CONSTRAINT campaigns_user_name_unique UNIQUE (user_sub, name)
 );
 `;
